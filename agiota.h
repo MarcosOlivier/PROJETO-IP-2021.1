1f01f404d9c130d6753598f4a96de12319a8fb74
LeandroLuiz02 <82337128+LeandroLuiz02@users.noreply.github.com>
2021-12-18 18:06:50 -0300

    Create agiota.h

---
+++ agiota.h
@@ -0,0 +1,31 @@
+// todos os structs que usamos no projeto organizado em uma file separada
+// **********************************************************************
+
+#include "raylib.h"
+
+typedef struct Inimigo{
+    Rectangle hitbox;       //hitbox do inimigo
+    bool vivo;              //Saber o estado do inimigo (vivo ou morto), precisamos do codigo da BALA. inimigo[i].vivo = vivo. !inimigo[i].vivo = morto;
+    Color cor;              //Cor da hitbox do inimigo (BLANK para ficar transparente)(hitbox)
+} Inimigo;
+
+Inimigo inimigo[5];
+
+typedef struct Mouse{
+    Rectangle hitbox;       //Hitbox do inimigo
+    Color cor;              //Cor da hitbox do player (BLANK para ficar transparente)
+} Mouse;
+
+Mouse mouse;                //Struct do mouse (Podia ser substituida por uma struct rectangle pro codigo ficar mais polido)
+
+typedef struct moeda{
+    Rectangle hitbox;       //Hitbox da moeda
+    bool vivo;              //Saber se a moeda existe
+    Color cor;              //Cor da hitbox da moeda (BLANK para ficar transparente)
+} Moeda;
+
+typedef struct Player
+{
+    Vector2 position;
+    Vector2 speed;
+} Player;
